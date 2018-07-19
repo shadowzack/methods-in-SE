db188bdefc9ea429ef13bf643597a5872ef5019e
mahmod hasan <battleforge-war@hotmail.com>
2018-07-19 19:53:17 +0300

    test texbox

--- methods-in-SE/main.cpp
+++ methods-in-SE/main.cpp
@@ -2,11 +2,15 @@
 #include "stdafx.h"
 #include"Control.h"
 #include <iostream>
+#include "TextBox.h"
 
 using namespace std;
 Focused* Focused::_instance = 0;
 int main()
 {
+	TextBox tmp(10);
+	tmp.setText(L"last");
+	tmp.setBorder(BorderType::Single);
 	cout << " hello world \n";
 	return 0;
 }
