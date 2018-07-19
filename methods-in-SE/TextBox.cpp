467d6957f72312a056324b18095ead29ba1e8e9c
mahmod hasan <battleforge-war@hotmail.com>
2018-07-19 19:45:49 +0300

    adding text box

---
+++ methods-in-SE/TextBox.cpp
@@ -0,0 +1,69 @@
+#include "stdafx.h"
+#include "TextBox.h"
+
+TextBox::TextBox(int width) {
+	_width = width;
+	_height = 2;
+	_top = 0;
+	_left = 0;
+	_layer = 0;
+	_cursorPositionx = 0;
+}
+
+void TextBox::mousePressed(int x, int y, bool isLeft) {
+	Control::getFocus()->restCursor();
+	Control::setFocus(*this);
+}
+void TextBox::keyDown(int keyCode, char charecter) {
+	auto _pos = Focused::instance()->getGraph()->getInfo();
+	this->_cursorPositionx = _pos.dwCursorPosition.X;
+
+	if (keyCode == VK_LEFT) {
+		if (_pos.dwCursorPosition.X > this->getLeft() + 1) {
+			this->_cursorPositionx--;
+			Focused::instance()->getGraph()->moveTo(_cursorPositionx, _pos.dwCursorPosition.Y);
+		}
+	}
+	else if (keyCode == VK_RIGHT) {
+		if (_pos.dwCursorPosition.X <= _text.size() + _left)
+			this->_cursorPositionx++;
+		Focused::instance()->getGraph()->moveTo(_cursorPositionx, _pos.dwCursorPosition.Y);
+	}
+	else if (keyCode == VK_BACK) {
+		if (_pos.dwCursorPosition.X == this->getText().size() + _left + 1) {
+			if (_text.size() != 0)
+				_text.pop_back();
+		}
+		if (_cursorPositionx > _left + 1)
+			Focused::instance()->getGraph()->moveTo(_cursorPositionx--, _pos.dwCursorPosition.Y);
+		else
+			Focused::instance()->getGraph()->moveTo(_cursorPositionx, _pos.dwCursorPosition.Y);
+	}
+	else {
+		if (_cursorPositionx < _left + getWidth()) {
+			char temp[1];
+			temp[0] = charecter;
+			if (_text.size() == 0) {
+				_text = (temp[0], temp[0]);
+			}
+			else
+				_text += (temp[0], temp[0]);
+			_cursorPositionx++;
+		}
+	}
+}
+
+bool TextBox::canGetFocus() {
+	return true;
+}
+
+void TextBox::draw(Graphics& g, int x, int y, size_t layer) {
+	if (layer == getLayer()) {
+		if (getVisibilty()) {
+			g.setBackground(this->getBackground());
+			g.setForeground(this->getForeground());
+			g.write(x + getLeft() + 1, getTop() + y + 1, _text + wstring(getWidth() - _text.size(), ' '));
+			Control::draw(g, x, y, layer);
+		}
+	}
+}
