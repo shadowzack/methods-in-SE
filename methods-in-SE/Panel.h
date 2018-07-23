818bf6385e0f5f695bc65501524dfdd6f5b051ee
mahmod hasan <battleforge-war@hotmail.com>
2018-07-23 14:53:17 +0300

    adding panel and button

---
+++ methods-in-SE/Panel.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Button.h"
+#include <vector>
+
+class Panel : public Control {
+protected:
+	vector<Control*> _controls;
+
+public:
+	Panel(int height = 20, int width = 20);
+	~Panel() {}
+	void AddControl(Control &control, int x, int y);
+	/* Control Methods*/
+	void draw(Graphics& g, int x, int y, size_t layer);
+	virtual void getAllControls(vector<Control*>* controls);
+	virtual bool canGetFocus();
+	virtual void mousePressed(int x, int y, bool isLeft);
+
+};
+
