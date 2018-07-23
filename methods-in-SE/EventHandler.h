cc6373e48d5e23510fc34e3baae85085ea50e421
mahmod hasan <battleforge-war@hotmail.com>
2018-07-23 15:16:53 +0300

    need fix event handler

---
+++ methods-in-SE/EventHandler.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Control.h"
+#include <Windows.h>
+#include "Graphics.h"
+
+class EventHandler
+{
+public:
+	EventHandler(DWORD input = STD_INPUT_HANDLE, DWORD output = STD_OUTPUT_HANDLE);
+	void run(Control &c);
+	virtual ~EventHandler();
+private:
+	Graphics _graphics;
+	HANDLE _console;
+	DWORD _consoleMode;
+	void moveFocus(Control &main, Control *focused);
+	void deepFocus(Control &main, Control *focused);
+};
+
