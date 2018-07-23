a0e8c6c4eb2a2885aa1e47dcd8b2503886f6086b
mahmod hasan <battleforge-war@hotmail.com>
2018-07-23 14:58:38 +0300

    a

--- methods-in-SE/Button.h
+++ methods-in-SE/Button.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "Label.h"
+
+
+struct MouseListener
+{
+	virtual void MousePressed(Control &control, int x, int y, bool isLeft) = 0;
+};
+
+
+class Button : public Label {
+protected:
+	MouseListener * _listener;
+public:
+	Button(int width);
+	void addListener(MouseListener &listener);
+	virtual bool canGetFocus() { return false; }
+	virtual void mousePressed(int x, int y, bool isLeft);
+	inline MouseListener& getListener();
+	virtual string className() { return "Button"; }
+
+};
+
+MouseListener& Button::getListener() {
+	return *_listener;
+}
